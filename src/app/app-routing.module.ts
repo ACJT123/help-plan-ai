3661a97b03114bb5cf95b3736ddad627b19203bd
ACJT123 <acjtacjt@gmail.com>
2024-09-04 10:38:04 +0800

    k

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,33 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthComponent } from './auth/auth.component';
-import { LoginComponent } from './auth/component/login/login.component';
-import { SignUpComponent } from './auth/component/sign-up/sign-up.component';
 
-const routes: Routes = [
-  {
-    path: 'auth',
-    component: AuthComponent,
-    children: [
-      {
-        path: 'login',
-        component: LoginComponent,
-        loadChildren: () =>
-          import('./auth/component/login/login.module').then(
-            (m) => m.LoginModule
-          ),
-      },
-      {
-        path: 'sign-up',
-        component: SignUpComponent,
-        loadChildren: () =>
-          import('./auth/component/sign-up/sign-up.module').then(
-            (m) => m.SignUpModule
-          ),
-      },
-    ],
-  },
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,27 @@ import {
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/component/login/login.component';
-import { AuthModule } from './auth/auth.module';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-import { ButtonComponent } from './resuable/button/button.component';
-import { SignUpComponent } from './auth/component/sign-up/sign-up.component';
-import { LinkComponent } from './resuable/link/link.component';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    SignUpComponent,
-    ButtonComponent,
-    LinkComponent,
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [
+    provideClientHydration(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'help-plan-ai-123',
+        appId: '1:395722538438:web:7ee779d37c11d34a6d86c7',
+        storageBucket: 'help-plan-ai-123.appspot.com',
+        apiKey: 'AIzaSyCiPQbIwJvjueV3dC_vTNHn5LslNaXu-yY',
+        authDomain: 'help-plan-ai-123.firebaseapp.com',
+        messagingSenderId: '395722538438',
+        measurementId: 'G-8PZJ5TS5LC',
+      })
+    ),
+    provideAuth(() => getAuth()),
   ],
-  imports: [BrowserModule, AppRoutingModule, AuthModule, ReactiveFormsModule],
-  providers: [provideClientHydration(), provideFirebaseApp(() => initializeApp({"projectId":"help-plan-ai-123","appId":"1:395722538438:web:7ee779d37c11d34a6d86c7","storageBucket":"help-plan-ai-123.appspot.com","apiKey":"AIzaSyCiPQbIwJvjueV3dC_vTNHn5LslNaXu-yY","authDomain":"help-plan-ai-123.firebaseapp.com","messagingSenderId":"395722538438","measurementId":"G-8PZJ5TS5LC"})), provideAuth(() => getAuth())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
